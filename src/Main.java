913106702fd60d981e5ea9240bf5c123a85bd45f
molochko <bel.sneshka@gmail.com>
2023-10-26 21:10:13 +0500

    Убраны сканеры,добавлен игнор

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,17 @@
-import java.util.Scanner;
 public class Main
 {
     public static void main(String[] args) {
-        Scanner in1 = new Scanner(System.in);
-        System.out.println("Введите ваш текуший баланс:");
-        int DenegNaSimkeSeychas = in1.nextInt();
-        Scanner in2 = new Scanner(System.in);
-        System.out.println("Введите сумму пополнения:");
-        int Popolnenie = in2.nextInt();
-        if (Popolnenie > 1000){
-            int ItogovayaSumma=(DenegNaSimkeSeychas + Popolnenie + Popolnenie/100);
-            System.out.println("Вы пополнили счет на " + Popolnenie + " рублей - бонус равен " + Popolnenie/100 + " рубля/ей, итоговая сумма на балансе " + ItogovayaSumma + " рубля/ей" );
+        int x = 322;
+        System.out.println("Ваш текуший баланс:" + x);
+        int y = 2473;
+        System.out.println("Сумма пополнения:" + y);
+        if (y > 1000){
+            int t=(int)(x + y + y/100);
+            System.out.println("Вы пополнили счет на " + y + " рублей - бонус равен " + y/100 + " рубля/ей, итоговая сумма на балансе " + t + " рубля/ей" );
         }
-        if (Popolnenie <= 1000){
-            int ItogovayaSumma2=(DenegNaSimkeSeychas + Popolnenie);
-            System.out.println("Вы пополнили счет на " + Popolnenie + " рублей - бонусы не начислены, итоговая сумма на балансе " + ItogovayaSumma2 + " рубля/ей" );
+        if (y <= 1000){
+            int t2=(int)(x + y);
+            System.out.println("Вы пополнили счет на " + x + " рублей - бонусы не начислены, итоговая сумма на балансе " + t2 + " рубля/ей" );
         }
-        in1.close();
-        in2.close();
     }
 }
