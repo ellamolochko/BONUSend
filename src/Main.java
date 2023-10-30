240792ff2015cac2f496901cf98a4253c54f40ac
molochko <bel.sneshka@gmail.com>
2023-10-30 21:32:48 +0500

    Changed int

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int x = 322;
-        System.out.println("Ваш текуший баланс:" + x);
-        int y = 2473;
-        System.out.println("Сумма пополнения:" + y);
-        if (y > 1000) {
-            int t = (int) (x + y + y / 100);
-            System.out.println("Вы пополнили счет на " + y + " рублей - бонус равен " + y / 100 + " рубля/ей, итоговая сумма на балансе " + t + " рубля/ей");
+        int balance = 322;
+        System.out.println("Ваш текуший баланс:" + balance);
+        int amount = 2473;
+        System.out.println("Сумма пополнения:" + amount);
+        if (amount > 1000) {
+            int t = (int) (balance + amount + amount / 100);
+            System.out.println("Вы пополнили счет на " + amount + " рублей - бонус равен " + amount / 100 + " рубля/ей, итоговая сумма на балансе " + t + " рубля/ей");
         }
-        if (y <= 1000) {
-            int t2 = (int) (x + y);
-            System.out.println("Вы пополнили счет на " + x + " рублей - бонусы не начислены, итоговая сумма на балансе " + t2 + " рубля/ей");
+        if (amount <= 1000) {
+            int t2 = (int) (balance + amount);
+            System.out.println("Вы пополнили счет на " + balance + " рублей - бонусы не начислены, итоговая сумма на балансе " + t2 + " рубля/ей");
         }
     }
 }
