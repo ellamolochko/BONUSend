fae8b4da29233e6e47d044ad82ca0e3b8745ddea
molochko <bel.sneshka@gmail.com>
2023-11-01 18:41:02 +0500

    changed

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ public class Main {
         int amount = 2473;
         System.out.println("Сумма пополнения:" + amount);
         if (amount > 1000) {
-            int t = (int) (balance + amount + amount / 100);
-            System.out.println("Вы пополнили счет на " + amount + " рублей - бонус равен " + amount / 100 + " рубля/ей, итоговая сумма на балансе " + t + " рубля/ей");
+            int replenishment = (int) (balance + amount + amount / 100);
+            System.out.println("Вы пополнили счет на " + amount + " рублей - бонус равен " + amount / 100 + " рубля/ей, итоговая сумма на балансе " + replenishment + " рубля/ей");
         }
         if (amount <= 1000) {
-            int t2 = (int) (balance + amount);
-            System.out.println("Вы пополнили счет на " + balance + " рублей - бонусы не начислены, итоговая сумма на балансе " + t2 + " рубля/ей");
+            int replenishment2 = (int) (balance + amount);
+            System.out.println("Вы пополнили счет на " + balance + " рублей - бонусы не начислены, итоговая сумма на балансе " + replenishment2 + " рубля/ей");
         }
     }
 }
