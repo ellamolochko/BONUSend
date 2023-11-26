2872a300a2674a744b1fd5688baec580c0adac6e
molochko <bel.sneshka@gmail.com>
2023-11-26 21:55:53 +0500

    reformate

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,8 @@ public class Main {
         if (amount > 1000) {
             int replenishment = (balance + amount + amount / 100);
             System.out.println("Вы пополнили счет на " + amount + " рублей - бонус равен " + amount / 100 + " рубля/ей, итоговая сумма на балансе " + replenishment + " рубля/ей");
-        }
-        else  {
-            int replenishment2 = (int) (balance + amount);
+        } else {
+            int replenishment2 = balance + amount;
             System.out.println("Вы пополнили счет на " + balance + " рублей - бонусы не начислены, итоговая сумма на балансе " + replenishment2 + " рубля/ей");
         }
     }
